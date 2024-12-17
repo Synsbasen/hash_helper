6d08f7b013421362dff7404580e3d6fc3c77fe55
Tobias Knudsen <tobias.knudsen@gmail.com>
2024-12-17 23:04:46 +0100

    Use switch instead of if-else

--- lib/hash_helper/percentage.rb
+++ lib/hash_helper/percentage.rb
@@ -4,14 +4,15 @@ module HashHelper
       total_sum = deep_sum
       return self if total_sum.zero?
 
-      transform_values do |v|
-        if v.is_a?(Hash)
-          v.transform_values_to_percentages(precision: precision)
-        elsif v.is_a?(Numeric)
-          percentage = (v.to_f / deep_sum * 100)
+      transform_values do |value|
+        case value
+        when Hash
+          value.transform_values_to_percentages(precision: precision)
+        when Numeric
+          percentage = (value / deep_sum * 100)
           precision ? percentage.round(precision) : percentage
         else
-          v
+          value
         end
       end
     end
