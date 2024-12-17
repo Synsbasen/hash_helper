5eaa6277e241407d127abef76a17b605bc058652
Tobias Knudsen <tobias.knudsen@gmail.com>
2024-12-17 23:00:28 +0100

    Simplify HashHelper::Percentage by removing sum_of_leaves

--- lib/hash_helper/percentage.rb
+++ lib/hash_helper/percentage.rb
@@ -1,31 +1,19 @@
 module HashHelper
   module Percentage
     def transform_values_to_percentages(precision: nil)
-      total_sum = sum_of_leaves
+      total_sum = deep_sum
       return self if total_sum.zero?
 
       transform_values do |v|
         if v.is_a?(Hash)
           v.transform_values_to_percentages(precision: precision)
         elsif v.is_a?(Numeric)
-          percentage = (v.to_f / total_sum * 100)
+          percentage = (v.to_f / deep_sum * 100)
           precision ? percentage.round(precision) : percentage
         else
-          v # Preserve non-numeric and nil values
+          v
         end
       end
-    end
-
-    def sum_of_leaves
-      sum do |_k, v|
-        if v.is_a?(Hash)
-          v.sum_of_leaves
-        elsif v.is_a?(Numeric)
-          v.to_f
-        else
-          0.0
-        end
-      end.to_f
     end
   end
 end
