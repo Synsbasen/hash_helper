04dc1eb87b8540daa000de8ab0b114200c414994
Tobias Knudsen <tobias.knudsen@gmail.com>
2024-12-17 17:40:35 +0100

    Hotfix: Simplify transform_values_to_percentages

--- lib/hash_helper/percentage.rb
+++ lib/hash_helper/percentage.rb
@@ -1,34 +1,31 @@
 module HashHelper
   module Percentage
     def transform_values_to_percentages(precision: nil)
-      total_sum = sum_of_leaves(self)
-      return self if total_sum.zero? # If total is zero, return original hash
+      total_sum = sum_of_leaves
+      return self if total_sum.zero?
 
-      calculate_percentages(self, total_sum, precision)
-    end
-
-    private
-
-    # Recursively calculate the total sum of all numeric leaf values
-    def sum_of_leaves(hash)
-      hash.sum do |_k, v|
-        v.is_a?(Hash) ? sum_of_leaves(v) : (v.is_a?(Numeric) ? v.to_f : 0.0)
-      end.to_f
-    end
-
-    # Recursively calculate percentages for each key
-    def calculate_percentages(hash, total_sum, precision)
-      hash.transform_values do |v|
+      transform_values do |v|
         if v.is_a?(Hash)
-          local_sum = sum_of_leaves(v)
-          calculate_percentages(v, local_sum, precision)
+          v.transform_values_to_percentages(precision: precision)
         elsif v.is_a?(Numeric)
           percentage = (v.to_f / total_sum * 100)
           precision ? percentage.round(precision) : percentage
         else
-          v # Preserve non-numeric and nil values as they are
+          v # Preserve non-numeric and nil values
         end
       end
+    end
+
+    def sum_of_leaves
+      sum do |_k, v|
+        if v.is_a?(Hash)
+          v.sum_of_leaves
+        elsif v.is_a?(Numeric)
+          v.to_f
+        else
+          0.0
+        end
+      end.to_f
     end
   end
 end
