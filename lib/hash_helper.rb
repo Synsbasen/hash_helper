0adf782ec6f7d661a4c1f2deccc64dcde0fdb35d
Tobias Knudsen <tobias.knudsen@gmail.com>
2024-12-17 15:23:03 +0100

    Add transform_values_to_percentages

--- lib/hash_helper.rb
+++ lib/hash_helper.rb
@@ -3,6 +3,7 @@
 require_relative "hash_helper/version"
 require_relative "hash_helper/deep_invert"
 require_relative "hash_helper/deep_normalize"
+require_relative "hash_helper/percentage"
 
 module HashHelper
 end
@@ -10,4 +11,5 @@ end
 class Hash
   include HashHelper::DeepInvert
   include HashHelper::DeepNormalize
+  include HashHelper::Percentage
 end
