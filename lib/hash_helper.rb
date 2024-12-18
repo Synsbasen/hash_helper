dda0aad53e9cc9d00979e89bb89a85c77b179a32
Tobias Knudsen <tobias.knudsen@gmail.com>
2024-12-18 10:01:55 +0100

    Add to_nested_h to arrays (#6)

--- lib/hash_helper.rb
+++ lib/hash_helper.rb
@@ -4,6 +4,7 @@ require_relative "hash_helper/version"
 require_relative "hash_helper/deep_invert"
 require_relative "hash_helper/deep_normalize"
 require_relative "hash_helper/percentage"
+require_relative "hash_helper/to_nested_h"
 
 module HashHelper
 end
@@ -12,4 +13,8 @@ class Hash
   include HashHelper::DeepInvert
   include HashHelper::DeepNormalize
   include HashHelper::Percentage
+end
+
+class Array
+  include HashHelper::ToNestedH
 end
